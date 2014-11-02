4c4bc03f2b3682600a88cc832b993695d96b2413
mlk007 <manavleslie@Manavs-MacBook-Pro.local>
2014-11-02 20:02:46 +0000

    Fixed imports

--- src/test/java/org/mlk007/kata/strat/controller/TestDataQueryController.java
+++ src/test/java/org/mlk007/kata/strat/controller/TestDataQueryController.java
@@ -1,6 +1,5 @@
 package org.mlk007.kata.strat.controller;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
